6fdc4e51d01b0e811143062d765a045e0934e045
Joona Tuunanen <joona@dublao.com>
2013-03-28 14:45:59 +0100

    Add user friendships and associated tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  should have_many(:user_friendships)
+  should have_many(:friends)
+
   test "a user should enter a first name" do
   	user = User.new
   	assert !user.save
@@ -40,5 +44,17 @@ class UserTest < ActiveSupport::TestCase
     user.password = user.password_confirmation = 'asdfasds'
     user.profile_name = "joona2"
     assert user.valid?
+  end
+
+  test "that no error is raised when trying to access a friend list" do
+    assert_nothing_raised do
+      users(:joona).friends
+    end
+  end
+
+  test "that creating friendships on a user works" do
+    users(:joona).friends << users(:mike)
+    users(:joona).friends.reload
+    assert users(:joona).friends.include?(users(:mike))
   end
 end
